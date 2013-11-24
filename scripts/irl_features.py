d5ac2ff4bf2859979fd5221bf84fb65d6d742395
Billy Okal <okal.billy@googlemail.com>
2013-11-24 13:29:14 +0100

    robot now moves

--- scripts/irl_features.py
+++ scripts/irl_features.py
@@ -74,15 +74,17 @@ def plan( weights, feature_type, feature_params, x1, y1, x2, y2, cell_size, robo
     next_cell = convert.from_world2( world_path[i] )
     if ( current_cell[:2] == next_cell[:2] ).all():
       i += 1
-    if not i < len( world_path ) or i > 1:
+    if not i < len( world_path ):# or i > 1:
+      sys.stderr.write( "Breaking with i:= (%d, %d)\n" % (i,  len(world_path) ) )
       break
     current[2:] = world_path[i][:2] - current[:2] 
     current[2:] = speed * current[2:] / np.linalg.norm( current[2:] )
     current[:2] += current[2:]
     interpolated_path.append( current * 1.0 )
-  if len( interpolated_path ) > 1:
-    sys.stderr.write( "Current: %f, %f\n" % ( robot[0], robot[1] ) )
-    sys.stderr.write( "Path[0]: %f, %f\n" % ( interpolated_path[1][0], interpolated_path[1][1] ) )
+
+  # if len( interpolated_path ) > 1:
+    # sys.stderr.write( "Current: %f, %f\n" % ( robot[0], robot[1] ) )
+    # sys.stderr.write( "Path[0]: %f, %f\n" % ( interpolated_path[1][0], interpolated_path[1][1] ) )
   return interpolated_path
 
 
