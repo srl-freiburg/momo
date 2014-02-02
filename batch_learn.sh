2f72dd23e65bf109dce71daeca870ae6db4c6a81
Dizan Vasquez <dix_ans@fastmail.fm>
2014-02-02 10:33:08 +0100

    Add batch learning script

---
+++ batch_learn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+bin/learn scene1 helbing stork 3.0 10 --ids=1 --delta=1.0 --path ~/Dropbox/software/freiburg/momo_data/
+bin/learn scene1 icra14 stork 3.0 10 --ids=1 --delta=1.0 --path ~/Dropbox/software/freiburg/momo_data/
+bin/learn scene1 anisotropic stork 3.0 10 --ids=1 --delta=1.0 --path ~/Dropbox/software/freiburg/momo_data/
+bin/learn scene1 kimlike stork 3.0 10 --ids=1 --delta=1.0 --path ~/Dropbox/software/freiburg/momo_data/
+bin/learn scene2 helbing stork 3.0 10 --ids=1 --delta=1.0 --path ~/Dropbox/software/freiburg/momo_data/
+bin/learn scene2 icra14 stork 3.0 10 --ids=1 --delta=1.0 --path ~/Dropbox/software/freiburg/momo_data/
+bin/learn scene2 anisotropic stork 3.0 10 --ids=1 --delta=1.0 --path ~/Dropbox/software/freiburg/momo_data/
+bin/learn scene2 kimlike stork 3.0 10 --ids=1 --delta=1.0 --path ~/Dropbox/software/freiburg/momo_data/
+bin/learn scene3 helbing stork 3.0 10 --ids=1 --delta=1.0 --path ~/Dropbox/software/freiburg/momo_data/
+bin/learn scene3 icra14 stork 3.0 10 --ids=1 --delta=1.0 --path ~/Dropbox/software/freiburg/momo_data/
+bin/learn scene3 anisotropic stork 3.0 10 --ids=1 --delta=1.0 --path ~/Dropbox/software/freiburg/momo_data/
+bin/learn scene3 kimlike stork 3.0 10 --ids=1 --delta=1.0 --path ~/Dropbox/software/freiburg/momo_data/
